895ca4faad1ee9c052d294562983279198b7b5e5
fadillahmuhammad <fadillahmuhammad1102@gmail.com>
2023-05-05 03:32:52 +0700

    aster

--- dashboard-pages/help_page.php
+++ dashboard-pages/help_page.php
@@ -1,1 +1,4 @@
-<h1>Ini adalah konten help pages</h1>
+<h1 style="padding: 5rem; background-color: whitesmoke; color:black;">Hubungi: <a target="_blank"
+        style="text-decoration: none;" href="https://wa.me/6285811013556"><span
+            style="color:#4e3a3a;">+6285811013556</span></a> untuk bertanya perihal website dan jika ingin menambah
+    fitur atau edit apapun mengenai website RestoDanauAbah, Terima kasih.</h1>
